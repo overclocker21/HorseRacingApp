3e73e2546f8557e3333e7482a5a5e8d18240675c
Serghei Piteli <Serghei.Piteli@morningstar.com>
2019-02-24 03:57:35 -0600

    - switched to port 80

--- server.js
+++ server.js
@@ -678,7 +678,7 @@ app.post('/privateleagues', (req, res) => {
 });
 
 //Starting server and listening to port 5000
-const port = 5000;
+const port = 80;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
