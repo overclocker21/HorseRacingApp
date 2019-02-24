c6b395374816928248ca2ab9b0216a245ff9e9bb
overclocker21 <javastarter100@gmail.com>
2019-02-24 14:51:47 -0600

    - formatting and removed merketplace popup banner

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.engine('handlebars', exphbs({
     defaultLayout: 'main',
     helpers: {
-        formatDateInUI: function (date) { 
+        formatDateInUI: function (date) {
             return dateFormat(date, "mmmm dS, yyyy");
         }
     }
@@ -74,14 +74,14 @@ app.post('/login-bla', async (req, res) => {
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({"facebookToken" : accessToken})
+        body: JSON.stringify({ "facebookToken": accessToken })
     }).then(response => {
         response.json().then(function (data) {
 
             console.log("Storing session id and user");
             app.locals.sessionid = data.session;
             app.locals.user = data.user;
-            res.send({ redirect: '/lobby' });     
+            res.send({ redirect: '/lobby' });
         });
     });
 });
@@ -94,7 +94,7 @@ app.get('/lobby', async (req, res) => {
 
     let sessionId = req.app.locals.sessionid;
     console.log("Session Id: " + sessionId);
-    
+
     //fetching leagues:
     await fetch('http://204.48.25.72:8080/functions/leagues/fetch', {
         method: "POST",
@@ -108,34 +108,34 @@ app.get('/lobby', async (req, res) => {
 
             let private = data.leagues.filter(function (el) {
                 return el.isPrivate === true;
-              });
+            });
 
             let open = data.leagues.filter(function (el) {
-            return el.isPrivate === false;
+                return el.isPrivate === false;
             });
 
             //reducing both open and private leagues count to 4
 
             let reducedPrivate = [];
 
-            if (private.length <= 4){
+            if (private.length <= 4) {
                 reducedPrivate = private;
             } else {
-                    for (let i = 0; i < 4; i++){
-                        reducedPrivate[i] = private[i];
+                for (let i = 0; i < 4; i++) {
+                    reducedPrivate[i] = private[i];
                 }
             }
 
             let reducedOpen = [];
 
-            if (open.length <= 4){
+            if (open.length <= 4) {
                 reducedOpen = open;
             } else {
-                    for (let i = 0; i < 4; i++){
-                        reducedOpen[i] = open[i];
+                for (let i = 0; i < 4; i++) {
+                    reducedOpen[i] = open[i];
                 }
             }
-            
+
             res.render("lobby", {
                 private_leagues: reducedPrivate,
                 open_leagues: reducedOpen
@@ -159,16 +159,16 @@ app.get('/leaguedetails/:id', async (req, res) => {
             "Authorization": sessionId,
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({"leagueId":leagueId})
+        body: JSON.stringify({ "leagueId": leagueId })
     }).then(response => {
-        response.json().then(function (data) {     
+        response.json().then(function (data) {
 
-            res.render("leaguedetails", { league: data.league});
+            res.render("leaguedetails", { league: data.league });
 
         }).catch((error) => {
             console.log(error);
         });
-    }); 
+    });
 });
 
 app.get('/participants/:id', async (req, res) => {
@@ -182,16 +182,16 @@ app.get('/participants/:id', async (req, res) => {
             "Authorization": sessionId,
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({"leagueId":leagueId})
+        body: JSON.stringify({ "leagueId": leagueId })
     }).then(response => {
-        response.json().then(function (data) {     
+        response.json().then(function (data) {
 
-            res.render("participants", { league: data.league});
+            res.render("participants", { league: data.league });
 
         }).catch((error) => {
             console.log(error);
         });
-    }); 
+    });
 });
 
 
@@ -216,21 +216,21 @@ app.get('/selectedrace/:id', async (req, res) => {
 
             let selected = data.races.filter(function (el) {
                 return el.id === req.params.id;
-              });
+            });
 
             //here we need to check if selected race is in the array of user's races
-            
+
             let enrolled = false;
 
-            for (let i = 0; i < userObject.bets.length; i++){
-                if (selected[0].id === userObject.bets[i].race){
+            for (let i = 0; i < userObject.bets.length; i++) {
+                if (selected[0].id === userObject.bets[i].race) {
                     enrolled = true;
                     break;
                 }
             }
 
             res.render("races/selectedrace", { race: selected[0], enrolled: enrolled });
-            
+
         }).catch((error) => {
             console.log(error);
         });
@@ -252,8 +252,7 @@ app.get('/marketplace', async (req, res) => {
         body: JSON.stringify({})
     }).then(response => {
         response.json().then(function (data) {
-            
-            req.flash('success_msg', 'Redirected to Horse Marketplace! Here you can buy horses and then add them to races');
+
             res.render("marketplace", { foundPlace: data.horses });
 
         }).catch((error) => {
@@ -284,9 +283,9 @@ app.get('/account', async (req, res) => {
         body: JSON.stringify({})
     }).then(response => {
         response.json().then(function (data) {
-            
+
             res.render("myaccount", { foundUser: data.user });
-            
+
         }).catch((error) => {
             console.log(error);
         });
@@ -307,9 +306,9 @@ app.get('/stables', async (req, res) => {
         body: JSON.stringify({})
     }).then(response => {
         response.json().then(function (data) {
-            
+
             res.render("mystables", { horses: data.user.horses });
-            
+
         }).catch((error) => {
             console.log(error);
         });
@@ -335,21 +334,21 @@ app.get('/addhorse/:id', async (req, res) => {
 
             let selected = data.races.filter(function (el) {
                 return el.id === req.params.id;
-              });
+            });
 
             let raceHorses = selected[0].horses;
 
             let userHorses = userObject.horses;
 
             //adding label for user owned horses so we can filter them on the frontend
-            for (let i = 0; i < userHorses.length; i++){
-                for (let j = 0; j < raceHorses.length; j++){
-                    if (userHorses[i]._id === raceHorses[j]._id){
+            for (let i = 0; i < userHorses.length; i++) {
+                for (let j = 0; j < raceHorses.length; j++) {
+                    if (userHorses[i]._id === raceHorses[j]._id) {
                         raceHorses[j]["bought"] = true;
                     }
                 }
             }
-            
+
             res.render("horses/addhorse", { horses: raceHorses });
 
         }).catch((error) => {
@@ -370,7 +369,7 @@ app.post('/sellhorse/:id', async (req, res) => {
             "Authorization": sessionId,
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({"horseId":horseId})
+        body: JSON.stringify({ "horseId": horseId })
     }).then(response => {
         response.json().then(function (data) {
             //updating global user object
@@ -395,11 +394,11 @@ app.post('/buyhorse/:id', async (req, res) => {
             "Authorization": sessionId,
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({"horseId":horseId})
+        body: JSON.stringify({ "horseId": horseId })
     }).then(response => {
         response.json().then(function (data) {
 
-            if (data.reason){
+            if (data.reason) {
                 req.flash('error_msg', 'The horse you are trying to buy is already in your stable!');
                 res.redirect('/marketplace');
             } else {
@@ -421,7 +420,7 @@ app.post('/addbet/:id', async (req, res) => {
 
     let horseId = req.params.id;
     console.log("Horse ID:" + horseId);
-    
+
     let leagueId = req.app.locals.selected_league;
     console.log("League ID: " + leagueId);
 
@@ -436,13 +435,15 @@ app.post('/addbet/:id', async (req, res) => {
             "Authorization": sessionId,
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({"horseId":horseId,
-    "leagueId":leagueId,"raceId":raceId})
+        body: JSON.stringify({
+            "horseId": horseId,
+            "leagueId": leagueId, "raceId": raceId
+        })
     }).then(response => {
         response.json().then(function (data) {
             console.log(data);
-            
-            if (data.reason === 'tooLate'){
+
+            if (data.reason === 'tooLate') {
                 req.flash('error_msg', 'The race ended already!');
                 res.redirect('/addhorse');
             } else {
@@ -471,29 +472,29 @@ app.get('/myraces', async (req, res) => {
         body: JSON.stringify({})
     }).then(response => {
         response.json().then(async (me) => { return me })
-        .then(async(me) => {
-            await fetch('http://204.48.25.72:8080/functions/races/fetch', {
-                method: "POST",
-                headers: {
-                    "Authorization": sessionId,
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({})
-            }).then(response => {
-                response.json().then(function (all_races) {
+            .then(async (me) => {
+                await fetch('http://204.48.25.72:8080/functions/races/fetch', {
+                    method: "POST",
+                    headers: {
+                        "Authorization": sessionId,
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({})
+                }).then(response => {
+                    response.json().then(function (all_races) {
 
-                    let enrolled_races = filterEnrolledRaces(all_races.races, me.user.bets);                            
+                        let enrolled_races = filterEnrolledRaces(all_races.races, me.user.bets);
 
-                    res.render("myraces", { enrolled_races: enrolled_races });
+                        res.render("myraces", { enrolled_races: enrolled_races });
 
-                }).catch((error) => {
-                    console.log(error);
+                    }).catch((error) => {
+                        console.log(error);
+                    });
                 });
+            })
+            .catch((error) => {
+                console.log(error);
             });
-        })
-        .catch((error) => {
-            console.log(error);
-        });
     });
 
 });
@@ -502,7 +503,7 @@ app.get('/myraces', async (req, res) => {
 app.get('/races/:id', async (req, res) => {
 
     let sessionId = req.app.locals.sessionid;
-    
+
     //get selected league id is and store in locals
     app.locals.selected_league = req.params.id;
 
@@ -514,12 +515,12 @@ app.get('/races/:id', async (req, res) => {
             "Authorization": sessionId,
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({"leagueId":league_id})
+        body: JSON.stringify({ "leagueId": league_id })
     }).then(response => {
         response.json().then(function (data) {
-            
+
             res.render("races/all_races", { races: data.league.races });
-            
+
         }).catch((error) => {
             console.log(error);
         });
@@ -549,9 +550,9 @@ app.get('/allopenleagues', async (req, res) => {
 
             let open = data.leagues.filter(function (el) {
                 return el.isPrivate === false;
-              });
+            });
 
-            res.render("openLeagues/all_open_leagues", {
+            res.render("openleagues/all_open_leagues", {
                 open_leagues: open
             });
 
@@ -579,9 +580,9 @@ app.get('/allprivateleagues', async (req, res) => {
 
             let private = data.leagues.filter(function (el) {
                 return el.isPrivate === true;
-              });
+            });
 
-            res.render("privateLeagues/all_private_leagues", {
+            res.render("privateleagues/all_private_leagues", {
                 private_leagues: private
             });
 
@@ -601,7 +602,7 @@ app.get('/privateleagues/add', (req, res) => {
 app.post('/openleagues', async (req, res) => {
 
     let sessionId = req.app.locals.sessionid;
-    
+
     const newOpenLeague = {
         "title": req.body.openTitle,
         "maxRaces": parseInt(req.body.numOfRaces),
@@ -621,17 +622,17 @@ app.post('/openleagues', async (req, res) => {
     }).then(response => {
         response.json().then(function (data) {
 
-            if (data.reason === "invalidDateRange"){
+            if (data.reason === "invalidDateRange") {
                 req.flash('error_msg', 'Please enter valid date range');
                 res.redirect('/openleagues/add');
-            } else if (data.reason === "startDateIsTooEarly"){
+            } else if (data.reason === "startDateIsTooEarly") {
                 req.flash('error_msg', 'Please give at least 3 days in advance for the start date so other players can join');
                 res.redirect('/openleagues/add');
             } else {
                 req.flash('success_msg', 'Open League Created!');
                 res.redirect('/lobby');
             }
-            
+
         }).catch((error) => {
             console.log(error);
         });
@@ -642,7 +643,7 @@ app.post('/openleagues', async (req, res) => {
 app.post('/privateleagues', (req, res) => {
 
     let sessionId = req.app.locals.sessionid;
-    
+
     const newPrivateLeague = {
         "title": req.body.privateTitle,
         "maxRaces": parseInt(req.body.numOfRaces),
@@ -662,14 +663,14 @@ app.post('/privateleagues', (req, res) => {
     }).then(response => {
         response.json().then(function (data) {
 
-            if (data.reason === "invalidDateRange"){
+            if (data.reason === "invalidDateRange") {
                 req.flash('error_msg', 'Please enter valid date range');
                 res.redirect('/privateleagues/add');
             } else {
                 req.flash('success_msg', 'Private League Created!');
                 res.redirect('/lobby');
             }
-            
+
         }).catch((error) => {
             console.log(error);
         });
